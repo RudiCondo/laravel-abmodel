44e0789567eb0846dcf8bd0934734f5a3c8dcc56
rudi <rudicondo2702@gmail.com>
2025-10-19 18:55:00 -0400

    normalizar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.landing')
 
 @section('title', 'Home')
-<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 @section('content')
     <h1>index</h1>
     <p>This is the home page of our awesome website.</p>
