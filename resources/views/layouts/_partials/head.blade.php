79f43ee48a36a4468137a4f66832cff06e89f5cd
rudi <rudicondo2702@gmail.com>
2025-10-25 19:54:42 -0400

    prueba

--- resources/views/layouts/_partials/head.blade.php
+++ resources/views/layouts/_partials/head.blade.php
@@ -3,7 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', 'ABMODEL')</title>
 
-  @vite(['resources/css/style.css', 'resources/js/auth.js'])
+  @vite(['resources/css/style.css', 'resources/js/modal-auth.js'])
 
   <link rel="icon" href="{{ Vite::asset('resources/img/favicon.png') }}" type="image/png">
 </head>
