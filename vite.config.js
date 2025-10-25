9d752b2f938e1bf1c7220c4153566e2f332f6338
rudi <rudicondo2702@gmail.com>
2025-10-25 19:47:48 -0400

    probando

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
                 'resources/css/style.css',
                 'resources/js/app.js',
                 'resources/js/home.js',
-                'resources/css/estilos-ceremoniales.css',
                 'resources/js/productos.js',
                 'resources/js/bootstrap.js',
                 'resources/js/cliente.js',
