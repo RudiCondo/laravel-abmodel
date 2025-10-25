75539648b976883813464bae6656ead9e4c684e8
rudi <rudicondo2702@gmail.com>
2025-10-25 19:46:03 -0400

    prueba final

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/home.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/style.css',
+                'resources/js/app.js',
+                'resources/js/home.js',
+                'resources/css/estilos-ceremoniales.css',
+                'resources/js/productos.js',
+                'resources/js/bootstrap.js',
+                'resources/js/cliente.js',
+                'resources/js/emprendedor.js',
+                'resources/js/modal-auth.js',
+            ],
             refresh: true,
         }),
     ],
