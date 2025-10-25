273d42fd08070c346f5a5d6a06b9cbe632b3ee1a
rudi <rudicondo2702@gmail.com>
2025-10-25 19:52:24 -0400

    probando

--- resources/views/layouts/_partials/head.blade.php
+++ resources/views/layouts/_partials/head.blade.php
@@ -3,7 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', 'ABMODEL')</title>
 
-  @vite(['resources/css/estilos-ceremoniales.css', 'resources/js/auth.js'])
+  @vite(['resources/css/style.css', 'resources/js/auth.js'])
 
   <link rel="icon" href="{{ Vite::asset('resources/img/favicon.png') }}" type="image/png">
 </head>
