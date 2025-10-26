9517a379afd5fc568979085889dd62f8e0a31360
rudi <rudicondo2702@gmail.com>
2025-10-25 20:12:21 -0400

    sacando vite a la wea

--- resources/views/layouts/_partials/head.blade.php
+++ resources/views/layouts/_partials/head.blade.php
@@ -5,5 +5,5 @@
 
   @vite(['resources/css/style.css', 'resources/js/modal-auth.js'])
 
-  <link rel="icon" href="{{ Vite::asset('img/Logo.jpg') }}" type="image/png">
+  <link rel="icon" href="{{ asset('img/Logo.jpg') }}" type="image/jpeg">
 </head>
