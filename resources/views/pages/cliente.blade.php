73589538638cdd4d448e494e62883ea38208961a
rudi <rudicondo2702@gmail.com>
2025-10-25 20:15:40 -0400

    sacando vite2

--- resources/views/pages/cliente.blade.php
+++ resources/views/pages/cliente.blade.php
@@ -33,16 +33,16 @@
     <h2>Explora por categoría</h2>
     <div class="categorias">
       <a href="/categorias/1" class="categoria-icon">
-        <img src="{{ Vite::asset('img/maquillaje.png') }}" alt="Maquillaje">
+        <img src="{{ asset('img/maquillaje.png') }}" alt="Maquillaje">
       </a>
       <a href="/categorias/2" class="categoria-icon">
-        <img src="{{ Vite::asset('img/skincare.png') }}" alt="Skincare">
+        <img src="{{ asset('img/skincare.png') }}" alt="Skincare">
       </a>
       <a href="/categorias/3" class="categoria-icon">
-        <img src="{{ Vite::asset('img/fragancias.png') }}" alt="Fragancias">
+        <img src="{{ asset('img/fragancias.png') }}" alt="Fragancias">
       </a>
       <a href="/categorias/4" class="categoria-icon">
-        <img src="{{ Vite::asset('img/accesorios.png') }}" alt="Accesorios">
+        <img src="{{ asset('img/accesorios.png') }}" alt="Accesorios">
       </a>
     </div>
   </section>
